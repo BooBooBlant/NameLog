e666cbf2a0f3ad57264a5c796f5becb8cf6cb7c7
zoodogood <zoodogood@gmail.com>
2024-04-07 09:52:06 +0300

    Add supabase client

---
+++ src/lib/supabase/client.js
@@ -0,0 +1,8 @@
+import { config } from "../../config.json.js";
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  config.creditails.SUPABASE_URL,
+  config.creditails.SUPABASE_ANON_KEY
+);
+
