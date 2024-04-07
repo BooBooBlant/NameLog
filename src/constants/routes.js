4d5a1735ce3d03033af5e996941f1d16835b30ab
zoodogood <zoodogood@gmail.com>
2024-04-07 09:35:06 +0300

    Add routes

---
+++ src/constants/routes.js
@@ -0,0 +1,33 @@
+export const routes = [
+  {
+    label: "Зоны приватов",
+    id: "zones_privates",
+    sections: ["guides"],
+  },
+  { label: "Батарейки", id: "batareiki", sections: ["guides"] },
+  { label: "Кланы", id: "clans", sections: ["guides"] },
+  { label: "Помощь проекту", id: "project_help", sections: ["guides"] },
+  {
+    label: "Спавн легенд/ультра бистов",
+    id: "spawn_legend",
+    sections: ["guides"],
+  },
+  {
+    label: "Цены на предметы",
+    id: "prices_of_items",
+    sections: ["guides"],
+  },
+  {
+    label: "Магазины и их валюта",
+    id: "stores_and_value",
+    sections: ["guides"],
+  },
+  { label: "Покевикторина", id: "poke_question", sections: ["guides"] },
+  { label: "Измерения", id: "dimension", sections: ["guides"] },
+  {
+    label: "Админы и Модеры",
+    id: "harakteristika",
+    sections: ["guides"],
+  },
+  { label: "Полезное", id: "helpful", sections: ["guides"] },
+];
