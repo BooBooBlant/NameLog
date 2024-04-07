8f1c577538575e2d2b2cfa1d992797b15a67c7fc
zoodogood <zoodogood@gmail.com>
2024-04-07 09:49:51 +0300

    Add on-user-join api point

--- api/on-user-join.mjs
+++ api/on-user-join.mjs
@@ -1,3 +1,15 @@
-export function GET(request) {
-	return new Response("123");
+import { supabase } from "../src/lib/supabase/client.js";
+
+export async function GET(request) {
+	const result = await supabase.from("user_join_counter").select();
+	if (result.error) {
+		throw result.error;
+	}
+	if (result.data.length === 0) {
+		await supabase.from("user_join_counter").insert({ id: 0 });
+	}
+
+	const previousCount = result.data[0]?.count || 0;
+	await supabase.from("user_join_counter").update({ count: previousCount + 1 }).eq("id", 0);
+	return new Response(previousCount);
 }
